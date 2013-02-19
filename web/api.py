7e8e98254564e6b4578d0540f2e3d1c3410a42cd
Jordan Yelloz <jordan@yelloz.me>
2013-02-18 21:00:48 -0800

    Added the beginnings of an API that wraps a git repository and a whoosh index.

--- web/api.py
+++ web/api.py
@@ -6,11 +6,31 @@ class Page(object):
         self.history = history
 
 
+class GitPages(object):
+
+    def __init__(self, repo, index):
+        self.repo = repo
+        self.index = index
+
+    def page(self, date, slug, ref):
+        pass
+
+    def history(self, page):
+        pass
+
+    def previous_page(self, page):
+        pass
+
+    def next_page(self, page):
+        pass
+
+
 def page(page_pk):
     return Page(
         page_pk,
         list(page_history(page_pk))
     )
+
 
 def page_history(page_pk):
     import random
