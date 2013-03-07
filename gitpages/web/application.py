7cf8c5163f7e52a30944c4ec1984543964fb96e5
Jordan Yelloz <jordan@yelloz.me>
2013-03-06 21:36:09 -0800

    Fixed some template content, added typogrify as a requirement, and enabled it.

--- gitpages/web/application.py
+++ gitpages/web/application.py
@@ -3,6 +3,8 @@
 from flask import Flask
 from flask_failsafe import failsafe
 
+from typogrify.templatetags.jinja_filters import register as register_typogrify
+
 from .converters import GitRefConverter, UuidConverter
 
 
@@ -14,6 +16,8 @@ def create():
     application = Flask(__name__)
     application.url_map.converters['git_ref'] = GitRefConverter
     application.url_map.converters['uuid'] = UuidConverter
+
+    register_typogrify(application.jinja_env)
 
     gitpages_web_ui = ui.create_blueprint()
     application.register_blueprint(gitpages_web_ui)
