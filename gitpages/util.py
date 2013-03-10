09e3ba1b2e7c88b6e109dff3a33e614dc464644d
Jordan Yelloz <jordan@yelloz.me>
2013-03-10 00:45:01 -0800

    Made cache storage configurable per application.

--- gitpages/util.py
+++ gitpages/util.py
@@ -8,7 +8,8 @@ http://flask.pocoo.org/snippets/5/
 import re
 from unidecode import unidecode
 from functools import wraps
-from werkzeug.contrib.cache import SimpleCache
+
+from flask import current_app
 
 _stripped_re = re.compile(r'[()]+')
 _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.;]+')
@@ -27,9 +28,6 @@ def slugify(text, delim=u'-'):
     return unicode(delim.join(result))
 
 
-_cache = SimpleCache()
-
-
 def cached(key, key_builder=None, timeout=5 * 60):
 
     def decorator(f):
@@ -37,17 +35,19 @@ def cached(key, key_builder=None, timeout=5 * 60):
         @wraps(f)
         def decorated_function(*args, **kwargs):
 
+            cache = current_app.config['CACHE']
+
             if key_builder:
                 cache_key = key % key_builder(args[0])
             else:
                 cache_key = key % args[0]
 
-            cached_value = _cache.get(cache_key)
+            cached_value = cache.get(cache_key)
             if cached_value is not None:
                 return cached_value
 
             value = f(*args, **kwargs)
-            _cache.set(cache_key, value, timeout=timeout)
+            cache.set(cache_key, value, timeout=timeout)
             return value
 
         return decorated_function
