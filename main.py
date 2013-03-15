cac93762a71c99437ef030674c2c458c378f33c7
Jordan Yelloz <jordan@yelloz.me>
2013-03-15 00:19:13 -0700

    Fixed main.py so it configures logging before the application is started and can be used with gunicorn.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import logging
 
 from gitpages.web.application import create
 
-if __name__ == '__main__':
+logging.basicConfig(level=logging.DEBUG)
+application = create()
 
-    logging.basicConfig(level=logging.DEBUG)
-    create().run(debug=True, host='127.0.0.1')
+if __name__ == '__main__':
+    application.run(debug=True, host='127.0.0.1')
