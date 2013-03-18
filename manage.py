b94471d746e359b2f11a3ebaf2fa43181ab107b8
Jordan Yelloz <jordan@yelloz.me>
2013-03-17 20:09:26 -0700

    Updated manage.py so it configures logging.

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+import logging
 from flask.ext.script import Manager
-
 from gitpages.web.application import create
 
-manager = Manager(create)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    manager = Manager(create)
     manager.run()
