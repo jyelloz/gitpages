69aa441edabf4722fc0e3079a6f4b1b41682917b
Jordan Yelloz <jordan@yelloz.me>
2013-03-31 13:34:54 -0700

    Added a new command for running a gunicorn server, based on: http://stackoverflow.com/questions/14566570/how-to-use-flask-script-and-gunicorn

--- gitpages/commands.py
+++ gitpages/commands.py
@@ -1,5 +1,5 @@
 from flask import current_app
-from flask.ext.script import Command
+from flask.ext.script import Command, Option
 
 from .web import ui
 
@@ -29,3 +29,49 @@ class BuildIndex(Command):
             repo=current_app.repo,
             ref=current_app.default_ref,
         )
+
+
+class GunicornServer(Command):
+
+    description = 'Run the app within Gunicorn'
+
+    def __init__(self, host='127.0.0.1', port=8000, workers=4):
+        self.port = port
+        self.host = host
+        self.workers = workers
+
+    def get_options(self):
+        return (
+            Option('-H', '--host',
+                   dest='host',
+                   default=self.host),
+
+            Option('-p', '--port',
+                   dest='port',
+                   type=int,
+                   default=self.port),
+
+            Option('-w', '--workers',
+                   dest='workers',
+                   type=int,
+                   default=self.workers),
+        )
+
+    def handle(self, app, host, port, workers):
+
+        from gunicorn.app.base import Application
+
+        class FlaskApplication(Application):
+
+            def init(self, parser, opts, args):
+                return dict(
+                    bind='{0}:{1}'.format(host, port),
+                    workers=workers,
+                    proc_name=app.config['SITE_TITLE'],
+                    preload_app=True,
+                )
+
+            def load(self):
+                return app
+
+        FlaskApplication().run()
