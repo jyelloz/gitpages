799f54721e39dd21725f0be1f127fce5cbe8eedd
Jordan Yelloz <jordan@yelloz.me>
2013-04-06 23:15:47 -0700

    Refactored blueprint initialization so it can be attached to an arbitrary existing Flask.

--- gitpages/web/application.py
+++ gitpages/web/application.py
@@ -11,9 +11,17 @@ from .converters import GitRefConverter, UuidConverter
 @failsafe
 def create(*args, **kwargs):
 
+    application = Flask(__name__, *args, **kwargs)
+
+    register_gitpages_blueprint(application)
+
+    return application
+
+
+def register_gitpages_blueprint(application):
+
     from . import ui
 
-    application = Flask(__name__, *args, **kwargs)
     application.url_map.converters['git_ref'] = GitRefConverter
     application.url_map.converters['uuid'] = UuidConverter
 
