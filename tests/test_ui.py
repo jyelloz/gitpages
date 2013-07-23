0a11902aa4ab2cffa79838ab09e9cf7a97f34862
Jordan Yelloz <jordan@yelloz.me>
2013-07-23 11:40:50 -0700

    Added some UI tests.

---
+++ tests/test_ui.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from pytz import timezone
+from werkzeug.contrib import cache
+
+from gitpages.web.application import create
+
+from .base import GitPagesTestcase
+
+
+class UITest(GitPagesTestcase):
+
+    def setup(self):
+
+        super(UITest, self).setup()
+
+        def get_ram_index(*args, **kwargs):
+            return self.index
+
+        app = create()
+        app.testing = True
+
+        app.config.update(
+            TIMEZONE=timezone('UTC'),
+            SITE_TITLE=u'GitPages',
+            GITPAGES_REPOSITORY=self.repo,
+            GITPAGES_DEFAULT_REF='refs/heads/master',
+            GITPAGES_ALLOWED_STATUSES=[u'published', u'draft'],
+            GITPAGES_INDEX=get_ram_index,
+            CACHE=cache.NullCache(),
+            DEBUG=True,
+        )
+
+        self.app = app
+
+    def teardown(self):
+        pass
+
+    def test_page_not_found(self):
+
+        with self.app.test_client() as ctx:
+
+            response = ctx.get('/archives/1111/11/11/non-existent-page/')
+            self.assert_equal(response.status_code, 404)
+
+    def test_page_found(self):
+
+        with self.app.test_client() as ctx:
+
+            response = ctx.get('/archives/2011/11/11/sample-page/')
+            self.assert_equal(response.status_code, 200)
