2ea0209c260a0ac0891b0612ab4a92e0a451b245
Jordan Yelloz <jordan@yelloz.me>
2014-08-26 08:51:39 -0700

    Incremented project version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='GitPages',
-    version='0.2-rc.5',
+    version='0.2-rc.6',
     license='GPLv2',
     url='https://jordan.yelloz.me',
     author='Jordan Yelloz',
