e406aef341d54479a494cd3771adf564f319dac0
Jordan Yelloz <jordan@yelloz.me>
2018-01-13 20:19:58 -0800

    compat: used _ENCODING constant

--- gitpages/util/compat.py
+++ gitpages/util/compat.py
@@ -2,8 +2,8 @@ _ENCODING = 'utf-8'
 
 
 def _text_to_bytes(text):
-    return text.encode('utf-8')
+    return text.encode(_ENCODING)
 
 
 def _bytes_to_text(bytestring):
-    return bytestring.decode('utf-8')
+    return bytestring.decode(_ENCODING)
